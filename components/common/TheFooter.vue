09fc9547074d87df5a9259b2129a550e55ec292a
Abernethy-BY <by15242952083@outlook.com>
2023-07-19 20:00:24 +0800

    fix footer router label

--- components/common/TheFooter.vue
+++ components/common/TheFooter.vue
@@ -2,7 +2,7 @@
  * @Author: Bianyu by15242952083@outlook.com
  * @Date: 2023-07-18 19:40:28
  * @LastEditors: Abernethy-BY by15242952083@outlook.com
- * @LastEditTime: 2023-07-19 19:52:33
+ * @LastEditTime: 2023-07-19 19:59:21
  * @FilePath: \Blog\components\common\TheFooter.vue
  * @Description:
  * Copyright (c) 2023 by Bianyu email: by15242952083@outlook.com, All Rights Reserved.
@@ -10,10 +10,10 @@
 <script setup lang="ts">
 interface FooterNavigationList { name: string; path: string }
 const footerNavigationList = ref<FooterNavigationList[]>([
-  { name: 'About', path: '/about' },
-  { name: 'Projects', path: '/projects' },
-  { name: 'Speaking', path: '/speaking' },
-  { name: 'Uses', path: '/uses' },
+  { name: 'about', path: '/about' },
+  { name: 'projects', path: '/projects' },
+  { name: 'speaking', path: '/speaking' },
+  { name: 'uses', path: '/uses' },
 ])
 </script>
 
@@ -21,7 +21,7 @@ const footerNavigationList = ref<FooterNavigationList[]>([
   <div w-full flex border-t-1 class="border-#ced6e0 dark:border-#57606f">
     <div class="w-1/2" flex items-center justify-center p-10>
       <div v-for="(item, index) in footerNavigationList" :key="index">
-        <span px-4 v-text="item.name" />
+        <span px-4 v-text="$t(item.name)" />
       </div>
     </div>
     <div class="w-1/2" flex flex-col items-center justify-center>
